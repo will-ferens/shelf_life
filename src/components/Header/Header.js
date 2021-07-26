226ad6651289532fa051ebe8605672c886379b27
_dream <will.ferens@gmail.com>
2021-07-26 15:55:30 -0600

    revised /public, index.html, deleted boilerplate

---
+++ src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledHeader = styled.header``;
+const HeaderTitle = styled.h1``;
+
+const Header = () => {
+  return (
+    <StyledHeader>
+      <HeaderTitle>Shelf Life</HeaderTitle>
+    </StyledHeader>
+  )
+}
+
+
+export default Header;
